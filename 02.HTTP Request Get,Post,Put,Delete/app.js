db23924e8dd69eef0a08f686b1476eafc0d7ef49
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-12-13 00:20:41 +0500

    http requests concept clear

--- 02.HTTP Request Get,Post,Put,Delete/app.js
+++ 02.HTTP Request Get,Post,Put,Delete/app.js
@@ -1,19 +1,40 @@
 import express from "express"
 
-console.log("hello");
-
-
 const app = express();
 
+app.use(express.json());
+
+let users = [];
+
 app.get(('/'), (req, res) => {
-    res.send("Hello World")  // bowser hamy sirf get ki request ko handle krke deta hai 
-})
-app.get(('/users'), (req, res) => {
-    res.send([{ name: "Syed Shahwaiz Qasim", age: '22', institute: 'SMIT' }])
+    res.send(`Hello World ${Date.now().toString(22)}`)  // bowser hamy sirf get ki request ko handle krke deta hai 
 })
 
-app.post("/posts", (req, res) => {
-    res.send({ type: "hello" }) // browser post ki request handle nahi krta  
+app.get(('/users'), (req, res) => {
+    res.send([{ user: users, message: 'user fetch successfully' }])
+})
+
+app.post("/users", (req, res) => {
+    // console.log("POST Request===>", req.body);
+    users.push({ id: Date.now().toString(22), ...req.body })
+    // console.log("Request===>", users)
+    res.send({ message: "user added successfully" }) // browser post ki request handle nahi krta  
+})
+
+app.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+    //  const userDelete = users.findIndex((user) => user.id === Number(id)) // first method
+    //  users.splice(userDelete, 1)
+    users = users.filter((obj) => obj.id !== id) // second method
+    res.send({ id: id, message: 'user deleted successfully' })
+})
+
+app.put(('/users/:id'), (req, res) => {
+    const { id } = req.params;
+    const userUpdate = users.findIndex((user) => user.id === id);
+    console.log("userUpdate", userUpdate);
+    users.splice(userUpdate, 1, { id: id, ...req.body })
+    res.send({ id: id, message: "user updated successfully" })
 })
 
 app.listen(3000, () => {
