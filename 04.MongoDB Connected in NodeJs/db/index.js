e972f116c87f797fb5292dc748857703957b1a67
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-12-17 11:41:00 +0500

    env package install

--- 04.MongoDB Connected in NodeJs/db/index.js
+++ 04.MongoDB Connected in NodeJs/db/index.js
@@ -1,6 +1,9 @@
+import 'dotenv/config'
 import mongoose from "mongoose"
 
-const url = `mongodb+srv://syedshahwaiz:shahwaiz123@lms.fdc0y.mongodb.net/?retryWrites=true&w=majority&appName=LMS`
+const url = process.env.MONGODB_URI;
+// console.log('url==>', url);
+
 mongoose.connect(url);
 
 export default mongoose;
