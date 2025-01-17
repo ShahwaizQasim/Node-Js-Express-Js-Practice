e504309949fcab6fa65e5e0f1f3dae862b2944ca
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2025-01-17 16:39:02 +0500

    CRUD

--- 06_Middlewares/src/modules/users/controllers/addUser.js
+++ 06_Middlewares/src/modules/users/controllers/addUser.js
@@ -1,11 +1,13 @@
-
+import { UserModel } from "../models/index.js";
 
 const AddUser = async (req, res) => {
     try {
-        console.log("User", req.body);
-
+        let addUserDB = await UserModel(req.body);
+        addUserDB = await addUserDB.save();
+        console.log("UserAdd", addUserDB);
+        res.status(200).send({ status: 200, message: "user added successfully", User: addUserDB })
     } catch (error) {
-
+        res.status(400).send({ status: 400, message: error })
     }
 }
 
--- 06_Middlewares/src/modules/users/index.js
+++ 06_Middlewares/src/modules/users/index.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import AddUser from "./controllers/addUser.js";
-
+import GetUser from "./controllers/getUsers.js";
+import UpdateUser from "./controllers/updateUser.js";
+import DeleteUser from "./controllers/deleteUser.js";
 
 const userRouter = Router();
 
-userRouter.get('/', (req,res) => {
-    res.send({message:"Api Run"});
-})
-
+userRouter.get('/', GetUser);
 userRouter.post('/', AddUser)
+userRouter.put('/:id', UpdateUser)
+userRouter.delete('/:id', DeleteUser)
 
 export default userRouter;
